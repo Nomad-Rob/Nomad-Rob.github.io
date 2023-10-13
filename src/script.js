ec8e2d721caf2d2ffc6101c90a25b4b123544476
Nomad-Rob <rfarley89@gmail.com>
2023-10-13 12:35:35 -0500

    random update

--- src/script.js
+++ src/script.js
@@ -1,15 +1,14 @@
-function toggleMenu() {
-    const menu = document.getElementById('menu-bar');
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
-  }
-  
-  // Optional: Submit contact form using Fetch API or any other method
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Add your AJAX form submit logic here
-  });
-  
+function downloadCV() {
+    // Implement the download CV functionality here
+    alert('Download CV button clicked!');
+}
+
+function getInTouch() {
+    // Implement the get in touch functionality here
+    alert('Get in touch button clicked!');
+}
+
+function moreAboutMe() {
+    // Implement the more about me functionality here
+    alert('More about me button clicked!');
+}
