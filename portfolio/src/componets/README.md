39121f23cc3b7d8bb5c00223ccfaf8ddf9d72073
Nomad-Rob <rfarley89@gmail.com>
2023-11-27 16:27:21 -0600

    Good base. need to fix nav bar, add projects, add random pictures, and clean up paddings

--- portfolio/src/componets/README.md
+++
@@ -1,1 +0,0 @@
-All components for the portfolio website
---
+++ src/carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  var images = [
+      '../src/assets/images/carousel/broken_wall.jpg',
+      '../src/assets/images/carousel/rob_city.jpg',
+      // Add more image paths here
+  ];
+
+  function changeImage() {
+      var randomImage = images[Math.floor(Math.random() * images.length)];
+      $('#carousel .carousel-item.active img').attr('src', randomImage);
+  }
+
+  setInterval(changeImage, 3000);
+});
